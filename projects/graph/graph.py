0c77bff9a33470793123f2559879d39744e1c82e
Ian McElroy <mcelroyian@gmail.com>
2020-08-10 18:02:46 -0500

    init

--- projects/graph/graph.py
+++ projects/graph/graph.py
@@ -13,33 +13,74 @@ class Graph:
         """
         Add a vertex to the graph.
         """
-        pass  # TODO
+        self.vertices[vertex_id] = set()
 
     def add_edge(self, v1, v2):
         """
         Add a directed edge to the graph.
         """
-        pass  # TODO
+        if v1 in self.vertices and v2 in self.vertices:
+            self.vertices[v1].add(v2)
+        else:
+            raise IndexError("nonexistent vertex")
 
     def get_neighbors(self, vertex_id):
         """
         Get all neighbors (edges) of a vertex.
         """
-        pass  # TODO
+        return self.vertices[vertex_id]
 
     def bft(self, starting_vertex):
         """
         Print each vertex in breadth-first order
         beginning from starting_vertex.
         """
-        pass  # TODO
+        #make queue
+        remaining = Queue()
+        #add starting node
+        remaining.enqueue(starting_vertex)
+        #create set to track visited
+        visited = set()
+        #while queue has items
+        while remaining.size() > 0:
+            #dequeue item
+            current = remaining.dequeue()
+            #add all items neighbords to queue
+            friends = self.get_neighbors(current)
+            for fr in friends:
+                if fr not in visited:
+                    remaining.enqueue(fr)
+            #add node to set
+            visited.add(current)
+            #print node
+            print(current)
 
     def dft(self, starting_vertex):
         """
         Print each vertex in depth-first order
         beginning from starting_vertex.
         """
-        pass  # TODO
+        #make stack
+        pile = Stack()
+        #add start
+        pile.push(starting_vertex)
+        #have set
+        seen = set()
+        #while things in stack
+        while pile.size() > 0:
+            #take top off stack
+            current = pile.pop()
+            #add to seen set
+            if current not in seen:
+                print(current)
+            seen.add(current)
+            #any friends not in visted set
+            for nb in self.get_neighbors(current):
+                if nb not in seen:
+                #add to stack
+                    pile.push(nb)
+            #print
+
 
     def dft_recursive(self, starting_vertex):
         """
