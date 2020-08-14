dd44773129a23dae769cdc33905cf5c3d50d76eb
Ian McElroy <mcelroyian@gmail.com>
2020-08-13 19:56:12 -0500

    convert to path

--- projects/adventure/adv.py
+++ projects/adventure/adv.py
@@ -1,6 +1,8 @@
 from room import Room
 from player import Player
 from world import World
+from util import Stack, Queue
+from graph import Graph
 
 import random
 from ast import literal_eval
@@ -10,11 +12,11 @@ world = World()
 
 
 # You may uncomment the smaller graphs for development and testing purposes.
-# map_file = "maps/test_line.txt"
-# map_file = "maps/test_cross.txt"
+#map_file = "maps/test_line.txt"
+map_file = "maps/test_cross.txt"
 # map_file = "maps/test_loop.txt"
 # map_file = "maps/test_loop_fork.txt"
-map_file = "maps/main_maze.txt"
+#map_file = "maps/main_maze.txt"
 
 # Loads the map into a dictionary
 room_graph=literal_eval(open(map_file, "r").read())
@@ -29,6 +31,69 @@ player = Player(world.starting_room)
 # traversal_path = ['n', 'n']
 traversal_path = []
 
+#Traverse Maze
+def solve():
+
+    g = {}
+    d = {
+        "n": "s",
+        "s": "n",
+        "e": "w",
+        "w": "e"
+    }
+    def add_vert(current, direction, next_room):
+        g[current][direction] = next_room
+        g[next_room][d[direction]] = current
+
+    pile = Stack()
+    pile.push(player.current_room.id)
+    seen = set()
+
+    while pile.size() > 0:
+        current = pile.pop()
+        if current not in g:
+            g[current] = {}
+        for exit in player.current_room.get_exits():
+            g[current][exit] = "?"
+        seen.add(current)
+
+        for exit in player.current_room.get_exits():
+            if g[current][exit] == "?":
+                #move to next room
+                player.travel(exit)
+                room = player.current_room.id
+                #add room to graph
+                if room not in g:
+                    g[room] = {}
+                #connect two rooms in graph
+                add_vert(current, exit, player.current_room.id)
+                #add to stack
+                if room not in seen:
+                    pile.push(room)
+                #add direction to my traversal
+                traversal_path.append(exit)
+    print(g)
+
+        
+        
+
+
+solve()
+
+
+    # while pile.size() > 0:
+    #     current = pile.pop()
+    #     seen.add(current)
+    #     if isinstance(current, int):
+    #         room = current
+    #     else:
+    #         room = list(current)[-1]
+    #     exits = player.current_room.get_exits()
+    #     for exit in exits:
+    #         if isinstance(current, int):
+    #             pile.push(tuple([current] + [exit]))
+    #         else:
+    #             pile.push(tuple(current.append(exit)))
 
 
 # TRAVERSAL TEST
@@ -51,12 +116,13 @@ else:
 #######
 # UNCOMMENT TO WALK AROUND
 #######
-player.current_room.print_room_description(player)
-while True:
-    cmds = input("-> ").lower().split(" ")
-    if cmds[0] in ["n", "s", "e", "w"]:
-        player.travel(cmds[0], True)
-    elif cmds[0] == "q":
-        break
-    else:
-        print("I did not understand that command.")
+
+# player.current_room.print_room_description(player)
+# while True:
+#     cmds = input("-> ").lower().split(" ")
+#     if cmds[0] in ["n", "s", "e", "w"]:
+#         player.travel(cmds[0], True)
+#     elif cmds[0] == "q":
+#         break
+#     else:
+#         print("I did not understand that command.")
---
+++ projects/adventure/graph.py
@@ -0,0 +1,186 @@
+"""
+Simple graph implementation
+"""
+from util import Stack, Queue  # These may come in handy
+
+class Graph:
+
+    """Represent a graph as a dictionary of vertices mapping labels to edges."""
+    def __init__(self):
+        self.vertices = {}
+
+    def add_vertex(self, vertex_id):
+        """
+        Add a vertex to the graph.
+        """
+        self.vertices[vertex_id] = set()
+
+    def add_edge(self, v1, v2):
+        """
+        Add a directed edge to the graph.
+        """
+        if v1 in self.vertices and v2 in self.vertices:
+            self.vertices[v1].add(v2)
+        else:
+            raise IndexError("nonexistent vertex")
+
+    def get_neighbors(self, vertex_id):
+        """
+        Get all neighbors (edges) of a vertex.
+        """
+        return self.vertices[vertex_id]
+
+    def bft(self, starting_vertex):
+        """
+        Print each vertex in breadth-first order
+        beginning from starting_vertex.
+        """
+        #make queue
+        remaining = Queue()
+        #add starting node
+        remaining.enqueue(starting_vertex)
+        #create set to track visited
+        visited = set()
+        #while queue has items
+        while remaining.size() > 0:
+            #dequeue item
+            current = remaining.dequeue()
+            #add all items neighbords to queue
+            friends = self.get_neighbors(current)
+            for fr in friends:
+                if fr not in visited:
+                    remaining.enqueue(fr)
+            #add node to set
+            visited.add(current)
+            #print node
+            print(current)
+
+    def dft(self, starting_vertex):
+        """
+        Print each vertex in depth-first order
+        beginning from starting_vertex.
+        """
+        #make stack
+        pile = Stack()
+        #add start
+        pile.push(starting_vertex)
+        #have set
+        seen = set()
+        #while things in stack
+        while pile.size() > 0:
+            #take top off stack
+            current = pile.pop()
+            #add to seen set
+            if current not in seen:
+                print(current)
+            seen.add(current)
+            #any friends not in visted set
+            for nb in self.get_neighbors(current):
+                if nb not in seen:
+                #add to stack
+                    pile.push(nb)
+            #print
+
+
+    def dft_recursive(self, starting_vertex):
+        """
+        Print each vertex in depth-first order
+        beginning from starting_vertex.
+
+        This should be done using recursion.
+        """
+        pass  # TODO
+
+    def bfs(self, starting_vertex, destination_vertex):
+        """
+        Return a list containing the shortest path from
+        starting_vertex to destination_vertex in
+        breath-first order.
+        """
+        pass  # TODO
+
+    def dfs(self, starting_vertex, destination_vertex):
+        """
+        Return a list containing a path from
+        starting_vertex to destination_vertex in
+        depth-first order.
+        """
+        pass  # TODO
+
+    def dfs_recursive(self, starting_vertex, destination_vertex):
+        """
+        Return a list containing a path from
+        starting_vertex to destination_vertex in
+        depth-first order.
+
+        This should be done using recursion.
+        """
+        pass  # TODO
+
+if __name__ == '__main__':
+    graph = Graph()  # Instantiate your graph
+    # https://github.com/LambdaSchool/Graphs/blob/master/objectives/breadth-first-search/img/bfs-visit-order.png
+    graph.add_vertex(1)
+    graph.add_vertex(2)
+    graph.add_vertex(3)
+    graph.add_vertex(4)
+    graph.add_vertex(5)
+    graph.add_vertex(6)
+    graph.add_vertex(7)
+    graph.add_edge(5, 3)
+    graph.add_edge(6, 3)
+    graph.add_edge(7, 1)
+    graph.add_edge(4, 7)
+    graph.add_edge(1, 2)
+    graph.add_edge(7, 6)
+    graph.add_edge(2, 4)
+    graph.add_edge(3, 5)
+    graph.add_edge(2, 3)
+    graph.add_edge(4, 6)
+
+    '''
+    Should print:
+        {1: {2}, 2: {3, 4}, 3: {5}, 4: {6, 7}, 5: {3}, 6: {3}, 7: {1, 6}}
+    '''
+    print(graph.vertices)
+
+    '''
+    Valid BFT paths:
+        1, 2, 3, 4, 5, 6, 7
+        1, 2, 3, 4, 5, 7, 6
+        1, 2, 3, 4, 6, 7, 5
+        1, 2, 3, 4, 6, 5, 7
+        1, 2, 3, 4, 7, 6, 5
+        1, 2, 3, 4, 7, 5, 6
+        1, 2, 4, 3, 5, 6, 7
+        1, 2, 4, 3, 5, 7, 6
+        1, 2, 4, 3, 6, 7, 5
+        1, 2, 4, 3, 6, 5, 7
+        1, 2, 4, 3, 7, 6, 5
+        1, 2, 4, 3, 7, 5, 6
+    '''
+    graph.bft(1)
+
+    '''
+    Valid DFT paths:
+        1, 2, 3, 5, 4, 6, 7
+        1, 2, 3, 5, 4, 7, 6
+        1, 2, 4, 7, 6, 3, 5
+        1, 2, 4, 6, 3, 5, 7
+    '''
+    graph.dft(1)
+    graph.dft_recursive(1)
+
+    '''
+    Valid BFS path:
+        [1, 2, 4, 6]
+    '''
+    print(graph.bfs(1, 6))
+
+    '''
+    Valid DFS paths:
+        [1, 2, 4, 6]
+        [1, 2, 4, 7, 6]
+    '''
+    print(graph.dfs(1, 6))
+    print(graph.dfs_recursive(1, 6))
